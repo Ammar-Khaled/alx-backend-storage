4d81d3ddc318ac2bbd8bd41e2923d2ec897789c6
Ammar Khaled Abdel Nour Ibrahim Ali <ammar.khaled.github@gmail.com>
2023-12-18 21:18:57 +0200

    Update 102-log_stats.py: use mongo aggregate

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,46 +4,47 @@ Improve 12-log_stats.py by adding the top 10 of the most present IPs
 in the collection nginx of the database logs:
   The IPs top must be sorted (like in the README example)
 """
-if __name__ == '__main__':
+from pymongo import MongoClient
 
-    from pymongo import MongoClient
 
-    connection_string = 'mongodb://127.0.0.1:27017/'
+def log_stats(mongo_collection):
+    """
+    Provides some stats about Nginx logs stored in MongoDB
+    """
+    result = mongo_collection.count_documents({})
+    print(f"{result} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        documents = mongo_collection.count_documents({"method": method})
+    print("Methods:")
+    print(f"\tmethod {method}: {documents}")
+    status = mongo_collection.count_documents({"method": "GET",
+                                              "path": "/status"})
+    print(f"{status} status check")
 
-    client = MongoClient(connection_string)
+    print("IPs:")
+    first_IPs = mongo_collection.aggregate([
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for ips in first_IPs:
+        count = ips.get("count")
+        ip_address = ips.get("ip")
+        print(f"\t{ip_address}: {count}")
 
-    logs = client.logs
 
-    nginx = logs.get_collection('nginx')
-
-    x = nginx.count_documents({})
-
-    methods = {'GET': 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-    ips = dict()
-    status_number = 0
-    for doc in nginx.find({'method':
-                          {'$in': ["GET", "POST", "PUT", "PATCH", "DELETE"]}}):
-        methods[doc['method']] += 1
-        if (doc['method'] == 'GET' and doc['path'] == '/status'):
-            status_number += 1
-
-    for doc in nginx.find({}):
-        if doc['ip'] in ips.keys():
-            ips[doc['ip']] += 1
-        else:
-            ips[doc['ip']] = 1
-
-    ips = dict(sorted(ips.items(), key=lambda ip: ip[1]))
-
-    print(f"{x} logs")
-    for k, v in methods.items():
-        print(f"    method {k}: {v}")
-    print(f'{status_number} status check')
-    print('IPs:')
-    i = 0
-    for k, v in reversed(list(ips.items())[-10:]):
-        if i < 10:
-            print(f'\t{k}: {v}')
-            i += 1
-        else:
-            break
+if __name__ == "__main__":
+    with MongoClient() as client:
+        collection = client.logs.nginx
+        log_stats(collection)
