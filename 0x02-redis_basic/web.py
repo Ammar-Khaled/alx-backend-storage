bb3427ae7f8001d38cbfb0efbbb2d8289b444d80
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-12-20 18:48:40 +0000

    Implement using decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,17 +2,31 @@
 """Implementing an expiring web cache and tracker"""
 import requests
 import redis
+from typing import Callable
+from functools import wraps
 
 r = redis.Redis()
+
+
+def track_requests(method: Callable) -> Callable:
+    """Decorator."""
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f'cached:{url}')
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        response = method(url)
+        r.setex(f"cached:{url}", 10, response)
+        return response
+
+    return wrapper
 
 
 def get_page(url: str) -> str:
     """Use the requests module to obtain the HTML content of
     a particular URL and returns it."""
-    r.incr(f"count:{url}")
-    cached_html = r.get(f'cached:{url}')
-    if cached_html:
-        return cached_html.decode('utf-8')
     response = requests.get(url)
-    r.setex(f'cached:{url}', 10, response.text)
     return response.text
